6534c34131a1779b5b6a1dfebe8069e312624058
Lethabo Masina <lemasin022@student.wethinkcode.co.za>
2026-06-25 22:44:48 +0200

    Add robot position and implement movement system

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import copy
+
+
 maze = [
     [1,1,1,1,1],
     [0,0,1,0,1],
@@ -6,4 +9,23 @@ maze = [
     [1,1,1,1,1]
 ]
 
-print(maze)
+x = 2
+y = 2
+
+while True:
+    maze_copy = copy.deepcopy(maze)
+    maze_copy[y][x] = 5
+    for row in maze_copy:
+        print(row)
+
+    prompt = input("move: ").lower()
+    if prompt == "north":
+        y -= 1
+    elif prompt == "south":
+        y += 1
+    elif prompt == "east":
+        x += 1
+    elif prompt == "west":
+        x -= 1
+    elif prompt == "quit":
+        break
