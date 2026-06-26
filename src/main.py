eaf62a1520c49457e0832792aeebed0adfbd5856
Lethabo Masina <lemasin022@student.wethinkcode.co.za>
2026-06-26 11:42:43 +0200

    Prevent movement through walls and add exit detection

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import copy
 
 maze = [
     [1,1,1,1,1],
-    [0,0,1,0,1],
+    [9,0,1,0,1],
     [1,0,0,1,1],
     [1,1,0,0,1],
     [1,1,1,1,1]
@@ -12,20 +12,41 @@ maze = [
 x = 2
 y = 2
 
+def is_obstacle(x, y):
+    if maze[y][x] == 1:
+        return True
+    
+def is_exit(x, y):
+    if maze[y][x] == 9:
+        return True    
+
 while True:
     maze_copy = copy.deepcopy(maze)
     maze_copy[y][x] = 5
     for row in maze_copy:
         print(row)
 
+    new_x = x
+    new_y = y
+
     prompt = input("move: ").lower()
     if prompt == "north":
-        y -= 1
+        new_y -= 1
     elif prompt == "south":
-        y += 1
+        new_y += 1
     elif prompt == "east":
-        x += 1
+        new_x += 1
     elif prompt == "west":
-        x -= 1
+        new_x -= 1
     elif prompt == "quit":
         break
+
+    if is_obstacle(new_x, new_y):
+        print("obstacle! try again")
+    elif is_exit(new_x, new_y):
+        x = new_x
+        y = new_y
+        print("Congratulations! You solved the maze")
+    else:
+        x = new_x
+        y = new_y
